175789325e3b5797d81d0465dcbad7f0a4536560
Giacomo Tosin <giacomo.tosin@marconi.loc>
2020-10-14 12:35:03 +0200

    secondo commit

--- threadpool/App.java
+++ threadpool/App.java
@@ -1,6 +1,29 @@
+class Processor implements Runnable {
+
+    private int id;
+
+    public Processor(int id) {
+        this.id = id;
+
+    }
+
+    public void run() {
+        System.out.println("Starding: " + id);
+
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+        System.out.println("Completed: " + id);
+    }
+}
+
 public class App {
     public static void main(String[] args) {
-        
+
 
     }
 
